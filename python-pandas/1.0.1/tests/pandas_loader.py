a1551382a5a658c607b4ed9d3ff70aef2591ca76
Alexis Lucattini <alexis.lucattini@umccr.org>
2020-02-28 12:11:37 +1100

    Turns out print statements were not required

--- python-pandas/1.0.1/tests/pandas_loader.py
+++ python-pandas/1.0.1/tests/pandas_loader.py
@@ -7,18 +7,16 @@ import tempfile
 
 # Initial data frame
 x = pd.DataFrame({"A": [1, 2, 3]})
-print(x)
 
 # Run a query (to ensure pd.core.computation is loaded)
 y = x.query("A==1")['A'].item()
-print(y)
 
 # Import an excel spreadsheet
 # Loads xlrd, xml.etree.cElementTree
 print(Path(os.path.dirname(__file__), "additional_files", "Book1.xlsx"))
 z = pd.ExcelFile(Path(os.path.dirname(__file__), "additional_files", "Book1.xlsx"))
+# Raed in a spreadsheet
 a = z.parse(header=None, sheet_name='foo')
-print(a)
 
 # Write to an output file
 # Loads pandas.io.formats.csvs
